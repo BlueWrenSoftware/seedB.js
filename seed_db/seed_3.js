8a1e5563e01bbb352ab7d561fbdcf63fcd7539bd
Dirk Koudstaal <dkgithub@fastmail.com>
2022-01-23 13:08:11 +1100

    seed_3.js - creates schema for seedB_3 IndexedDB seed_3_data.js - creates data for the seedB_3 database seed_3_update.js - updates record with id = 1 as a demo

--- seed_db/seed_3.js
+++ seed_db/seed_3.js
@@ -1,89 +1,15 @@
+// Create IndexedDB schema with db name seedB_3
+
 let db;
 
-let request = window.indexedDB.open('seedDB_autoincrement', 1);
+let request = window.indexedDB.open('seedB_3', 1);
 request.onupgradeneeded = function(event) {
     // handle the upgradeneeded event
     db = event.target.result;
-    db.createObjectStore('packets', {keyPath:"pktId", autoIncrement: true});
-};
-
-request.onsuccess = function(event) {
-    db = event.target.result;
+    db.createObjectStore('entity', {keyPath:"Id", autoIncrement: true});
     db.onerror = function(event) {
         // Generic error handler for all errors targeted at this database's requests!
         console.error("Database error: " + event.target.errorCode);
       };
-    
-    let transaction = db.transaction('packets', 'readwrite');
-    let store = transaction.objectStore('packets');
-    store.put({
-        metadata: {
-            name: ""
-            ,recordtype: "seedpacket"
-            ,ORcode: ""
-        }
-        ,variety: "Curly Kale"
-        ,store: "Coldroom"
-        ,weight: 20
-        ,tag: []
-    });
-    store.put({
-        metadata: {
-            name: ""
-            ,recordtype: "seedpacket"
-            ,QRcode: ""
-        }
-       ,variety: "Curly Kale"
-        ,store: "Coldroom"
-        ,weight: 4
-        ,tag: []
-    });
-    store.put({
-        metadata: {
-            name: ""
-            ,recordtype: "seedpacket"
-            ,QRcode:""
-        }
-        ,variety: "Carrots"
-        ,store: "Coldroom"
-        ,weight: 10
-        ,tag: ["vegetable","Potting Shed"]
-    });
-    store.put({
-        metadata: {
-            name: ""
-            ,recordtype: "seedpacket"
-            ,QRcode:""
-        }
-        ,variety: "Carrots"
-        ,store: "Coldroom"
-        ,weight: 5
-        ,tag: ["vegetable"]
-    });
-    store.put({
-        metadata: {
-            name: ""
-            ,recordtype: "bulbs"
-            ,QRcode:""
-        }
-        ,variety: "Tulips"
-        ,store: "Coldroom"
-        ,weight: 100
-        ,number: 100
-        ,tag: ["flower"]
-    });
-    store.put({
-        metadata: {
-            name: ""
-            ,recordtype: "Seed Groups"
-            ,QRcode:""
-        }
-        ,seedGroups: ["Vegetables", "Flowers", "Herbs", "Fruits"]
-        ,description: "Groups for all the seeds kept"
-        ,tag: ["groups"]
-    });
-    transaction.oncomplete = ()=>{
-        db.close();
-    }
-
-}
+    db.close()
+};
--- seed_db/seed_3_update.js
+++ seed_db/seed_3_update.js
@@ -1,33 +1,33 @@
 let db;
 
-let request = window.indexedDB.open('seedDB_autoincrement', 1);
+let request = window.indexedDB.open('seedB_3', 1);
 request.onupgradeneeded = function(event) {
     // handle the upgradeneeded event
     db = event.target.result;
-    //db.createObjectStore('packets', {autoIncrement: true});
-};
+};    
 request.onsuccess = function(event) {
     db = event.target.result;
     db.onerror = function(error) {
         // Generic error handler for all errors targeted at this database's requests!
         console.log(error)//("Database error: " + event.target.errorCode);
-      };
+    };
     
-    let transaction = db.transaction('packets', 'readwrite');
-    let store = transaction.objectStore('packets');
+    let transaction = db.transaction('entity', 'readwrite');
+    let store = transaction.objectStore('entity');
     store.put({
         metadata: {
             name: ""
             ,recordtype: "seedpacket"
             ,ORcode: ""
         }
-        ,pktId : 1
+        ,Id : 1
         ,variety: "Curly Kale"
         ,store: "Coldroom"
         ,weight: 500
         ,tag: []
     });
-    transaction.oncomplete = ()=>{
+    
+    transaction.oncomplete = function() {
         db.close();
-    }
-}
+    };
+};
