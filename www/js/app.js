afe3920c9b74be77a45e028b14d0f66cdb184b89
Dirk Koudstaal <dkgithub@fastmail.com>
2022-07-16 10:17:02 +1000

    New error page if db does not exist. Changed most onclick to enventListener

--- www/js/app.js
+++ www/js/app.js
@@ -18,7 +18,7 @@ class UI {
 
   // Delete rows first before entering updated data
   static displaySeeds(seedTable) {
-      const table = document.querySelector('#seed-list');
+    const table = document.querySelector('#seed-list');
     while (table.rows.length > 0) {
       table.deleteRow(0);
     };
@@ -60,7 +60,6 @@ class UI {
     document.querySelector('#seedDatePacked').value = '';
     document.querySelector('#timeStamp').value = '';
     document.querySelector('#seedNotes').value = '';
-
   }
 
   static selectPages(pageSelected) {
@@ -91,17 +90,15 @@ class UI {
       document.querySelector("#edit-page").style.display = "";
       document.querySelector("#read-write-page").style.display = "none";
       document.querySelector("#instructions-page").style.display = "none";
-      //document.querySelector("#dbError").style.display = "none"
     }
     else if (pageSelected === "readWritePage") {
       document.title = "Backup";
-      document.querySelector("#js-db-error").style.display = "none";
       document.querySelector("#retrieve-data-button").style.display = "";
       document.querySelector("#home-page").style.display = "none";
       document.querySelector("#edit-page").style.display = "none";
       document.querySelector("#read-write-page").style.display = "";
       document.querySelector("#instructions-page").style.display = "none";
-      //document.querySelector("#dbError").style.display = "none"
+      document.querySelector("#dbError").style.display = "none"
     }
     else if (pageSelected === "instructionsPage") {
       document.title = "Instructions";
@@ -109,7 +106,6 @@ class UI {
       document.querySelector("#edit-page").style.display = "none";
       document.querySelector("#read-write-page").style.display = "none";
       document.querySelector("#instructions-page").style.display = "";
-      //document.querySelector("#dbError").style.display = "none"
     }
     else if (pageSelected === "dbError") {
       document.title = "Error";
@@ -119,7 +115,6 @@ class UI {
       document.querySelector("#read-write-page").style.display = "none";
       document.querySelector("#instructions-page").style.display = "none";
       document.querySelector("#read-write-page").style.display = "";
-      
     }
   }
 
@@ -169,6 +164,17 @@ class UI {
     console.log(sortState);
   }
 
+  static locateBtnHomePage(e) {
+    if (e.target === foundBtnHomePage)
+      {
+        fileNotes.innerHTML = "";
+        console.log("file notes deleted");
+        backupNotes.innerHTML = "";
+      }; 
+      console.log('Home Page selected');
+      UI.selectPages('homePage');
+  }
+
   // Should be in Store Class?
   static editSeed(record, editPage) {
     UI.clearFields();
@@ -184,25 +190,20 @@ class UI {
 // Store Class: Handles Storage 
 class Store {
   static corruptDB() {
+    //errorMsg.innerHTML  = "";
     const req = indexedDB.deleteDatabase('seedB');
     req.onsuccess = function () {
       console.log("Deleted database successfully");
-      const msg = document.getElementById('js-msg');
-      msg.insertAdjacentHTML('beforeend',
-      '<br>Deleted corrupted database.');
+      msgInstallBb.innerHTML += ('<li>=> Deleted corrupted database.</li>');
       Store.openDB();
     };
     req.onerror = function () {
       console.log("Couldn't delete database");
-      const msg = document.getElementById('js-msg');
-      msg.insertAdjacentHTML('beforeend',
-      '<br>Could not delete database');
+      msgInstallBb.innerHTML += ('<li>=> Could not delete database</li>');
     };
     req.onblocked = function () {
       console.log("Couldn't delete database due to the operation being blocked");
-      const msg = document.getElementById('js-msg');
-      msg.insertAdjacentHTML('beforeend',
-      '<br>Could not delete database due to the operation being blocked');
+      msgInstallBb.innerHTML += ('<li>=> Could not delete database due to the operation being blocked</li>');
     };
   }
 
@@ -217,15 +218,12 @@ class Store {
       store.createIndex('timeStamp', 'timeStamp', { unique: false });
       console.log('Index Creation Successful');
       console.log('The new database version number is = ' + event.newVersion);
-      const msg = document.getElementById('js-msg');
-      msg.insertAdjacentHTML('beforeend',
-      '<br>New SeedB created.');
-      const version = ('The new database version number is ' + event.newVersion + '.');
-      msg.insertAdjacentHTML('beforeend',
-      '<br>' + version);
+      const version = 'The new database version number is = ' + event.newVersion;
+      msgInstallBb.innerHTML += ('<li>=> New SeedB created.</li>');
+      msgInstallBb.innerHTML += ('<li>=> ' + version + '</li>');
     }
   }
- 
+
   static editAddRecord(mode) {
     console.log(mode);
     // Get form values
@@ -265,7 +263,7 @@ class Store {
 
       // Show success message
       UI.showAlert('Seed Packet Added', 'success', '#pkt-message', '#insert-form-alerts');
-      // Clear form fields
+      // Clear form fieldsBackupNotifications
       UI.clearFields();
       if (mode === 'editSeedPkt') {
         UI.selectPages('homePage');
@@ -325,7 +323,25 @@ class Store {
       }
     }
   }
-} 
+  static dbExist() {
+    const request = window.indexedDB.open('seedB');
+    request.onsuccess = function(e) {
+      const db = e.target.result;
+      console.log('db is open');
+      if (!db.objectStoreNames.contains('collection')) {
+        //db.close();
+        console.log('no object store');
+        UI.selectPages("dbError");
+        db.close();
+      }
+      else if (db.objectStoreNames.contains('collection')) {
+        console.log('Object store is named collection');
+        UI.selectPages('homePage');
+      }
+      // need final comment screen must not have captured some other error!
+    }
+  }
+}
 
 // Data
 class Data {
@@ -333,13 +349,12 @@ class Data {
     openDbPromise().then(
       db => {
         return new Promise((resolve, reject) => {
-          //request = db.transaction('collection').objectStore('collection').openCursor();
           const transaction = db.transaction(['collection'], "readonly");
           const store = transaction.objectStore('collection');
           let cursorRequest;
           let records = [];
           cursorRequest = store.openCursor();
-  
+
           cursorRequest.onsuccess = event => {
             let cursor = event.target.result;
             if (cursor) {
@@ -352,6 +367,8 @@ class Data {
             }
             else {
               resolve(records);
+              console.log('All SeedB data retrieved');
+              fileNotes.innerHTML += '<li>=> All SeedB data retrieved</li>';
             }
           }
           cursorRequest.onerror = (event) => {
@@ -365,31 +382,37 @@ class Data {
         const text = JSON.stringify(records, null, 2);
         console.log(text);
         let time = Data.fileTime() + ".txt";
-        var filename = "seedB-" + time;
+        const filename = "seedB-" + time;
+        console.log(filename);
+        fileNotes.innerHTML += '<li>=> Backup file name is: ' + filename + '</li>';
         Data.download(filename, text);
       });
   };
   static download(filename, textInput) {
-    var element = document.createElement('a');
+    const element = document.createElement('a');
     element.setAttribute('href', 'data:text/plain;charset=utf-8, ' + encodeURIComponent(textInput));
     element.setAttribute('download', filename);
     document.body.appendChild(element);
     element.click();
     document.body.removeChild(element);
+    console.log('Download complete');
+    fileNotes.innerHTML += '<li>=> Download is completed.</li>';
   };
   static fileTime() {
     const date = new Date();
     const year = date.getFullYear().toString();
-    const month = "0" + (date.getMonth() + 1).toString();
-    const day = "0" + (date.getDay() + 1).toString();
+    const month = "0" + (date.getMonth() + 1)
+    const day = "0" + date.getDate();
     const hours = "0" + date.getHours();
     const minutes = "0" + date.getMinutes();
     //const seconds = "0" + date.getSeconds();
     const formattedTime = `${year.slice(-2)}${month.slice(-2)}${day.slice(-2)}-${hours.slice(-2)}${minutes.slice(-2)}`;
+    console.log(date, day, month, year);
+    console.log(formattedTime);
     return formattedTime;
   };
 
-  static backUp() {
+  static backup() { // all the events have to be taken out!
     const fileSelect = document.getElementById("js-file-select");
     const extractFileData = document.getElementById("js-input-file-data");
     fileSelect.addEventListener("click", function () {
@@ -400,6 +423,12 @@ class Data {
     extractFileData.onchange = function () {
       let dataFile = [];
       const file = this.files[0];
+      console.log(file);
+      console.log(file.name);
+      console.log(file.lastModifiedDate.toString().slice(0,24));
+      backupNotes.innerHTML += '<li>=> Backup file used:  '+file.name+'</li>';
+      backupNotes.innerHTML += '<li>=> Backup file was created on:  '+ file.lastModifiedDate.toString().slice(0, 24) + '</li>';
+      backupNotes.innerHTML += '<li>=> Backup file now merged with seed list already in db</li>';
       const reader = new FileReader();
       reader.onload = function (progressEvent) {
         //console.log(this.result);
@@ -412,6 +441,7 @@ class Data {
     };
   };
 }
+
 // End of Classes
 
 // Global variables
@@ -419,21 +449,56 @@ let oldSortOn = '';
 let oldSortOrder = 'prev';
 let toTop = document.getElementById("js-page--to-top"); //Get the button
 let toBottom = document.getElementById("js-page--to-bottom"); //Get the button
-let deletedPkts = []; // session storage of deleted seed packets
-let sortState = []; //trying to keep track of sort config before edit of record
 
-// Functions and events
+//let deletedPkts = []; session storage of deleted seed packets
+//let sortState = []; trying to keep track of sort config before edit of record
+
+const htmlId = id => document.getElementById(id);
+const foundBtnHomePage = document.getElementById('findBtnHomePage');
+const fileNotes = document.getElementById('fileNotifications');
+const backupNotes = document.getElementById('backupNotifications');  
+const errorMsg = document.getElementById("dbError");
+const msgInstallBb = document.getElementById('installDbMsg');
+
 // Restore data from backup file
-Data.backUp();
-// Event Open DB
-document.addEventListener('DOMContentLoaded', UI.selectPages("homePage"));
+Data.backup(); // have to sort the events in that function
+
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = () => { UI.scrollEvent() };
-window.onload = () => {dbExist()};
+
+// Load IndexedDB and check for right store
+window.onload = () => { Store.dbExist() };
+
+//Events
 // Event Menu
 document.querySelector(".js-menu-hamburger").addEventListener("click", UI.menu);
+// Menu selection events
+htmlId('eventHomePage').addEventListener('click', () => { UI.selectPages('homePage') }, false);
+htmlId('eventPktPage').addEventListener('click', () => { UI.selectPages('newPktPage') }, false);
+htmlId('eventReadWritePage').addEventListener('click', () => { UI.selectPages('readWritePage') }, false);
+htmlId('eventInstrPage').addEventListener('click', () => { UI.selectPages('instructionsPage') }, false);
 
-function openDbPromise() {
+// Sort table columns events
+htmlId('sortSeedGroup').addEventListener('click', () => { UI.refreshTable('seedGroup') }, false);
+htmlId('sortVariety').addEventListener('click', () => { UI.refreshTable('variety') }, false);
+htmlId('sortPktId').addEventListener('click', () => { UI.refreshTable('pktId') }, false);
+htmlId('sortDatePacked').addEventListener('click', () => { UI.refreshTable('seedDatePacked') }, false);
+
+// Button & input events
+htmlId('btnSubmitRecord').addEventListener('click', () => { Store.editAddRecord('editSeedPkt') }, false);
+htmlId('btnDeleteRecord').addEventListener('click', () => { Store.deleteRecord() }, false);
+htmlId('btnNewRecord').addEventListener('click', () => { Store.editAddRecord('newPktPage') }, false);
+htmlId('btnRetrieveData').addEventListener('click', () => { Data.retrieveAll() }, false);
+htmlId('btnReinstall').addEventListener('click', () => { Store.corruptDB() }, false);
+htmlId('js-page--to-bottom').addEventListener('click', () => { UI.scrollToBottom() }, false);
+htmlId('js-page--to-top').addEventListener('click', () => { UI.scrollToTop() }, false);
+
+// Buttons & inputs all opening Home Page (same class)
+document.querySelectorAll('.btnHomePage')
+  .forEach(btn => btn.addEventListener('click', (e) => { UI.locateBtnHomePage(e) }, false));
+
+// Functions
+  function openDbPromise() {
   return new Promise((resolve, reject) => {
     const request = window.indexedDB.open('seedB', 1);
     request.onsuccess = (event) => {
@@ -469,7 +534,7 @@ function fetchSortDataPromise(db, sortOn = 'variety', sortOrder = 'next') {
       }
     }
     cursorRequest.onerror = (event) => {
-      reject(event.target.errorCode); 
+      reject(event.target.errorCode);
     }
   })
 };
@@ -503,31 +568,3 @@ function editSeedPkt(pktId) {
       UI.editSeed(record, 'editSeedPkt');
     });
 };
-
-// Latest code for downloading and uploading seedB data
-// code for creating backup files is now in Data Class
-
-// Upload backup data to seedB
-/* const fileSelect = document.getElementById("js-file-select");
-const extractFileData = document.getElementById("js-input-file-data");
-fileSelect.addEventListener("click", function () {
-  if (extractFileData) {
-    extractFileData.click();
-  }
-}, false); */
-
-
-/* extractFileData.onchange = function () {
-  let dataFile = [];
-  const file = this.files[0];
-  const reader = new FileReader();
-  reader.onload = function (progressEvent) {
-    //console.log(this.result);
-    dataFile = JSON.parse(this.result);
-    console.log(dataFile);
-    Store.addData(dataFile);
-    //console.log(dataFile[4]);
-  };
-  reader.readAsText(file);
-}; */
-
