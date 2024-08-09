59dd0a6f7bf23ab4641c0900d28c7de13326f6b3
dkoudstaal <dkgithub.fastmail.com>
2024-08-10 01:24:59 +1000

    updated main.js configuration

--- www/electron/main.js
+++ www/electron/main.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
   const win = new BrowserWindow({
-    autoHideMenuBar: true,
+    /*autoHideMenuBar: true,*/
     width: 800,
     height: 600
   })
